55a77f6deed18a081105d53948816b013ecb2d58
squishedfox <squishedfox@protonmail.com>
2026-05-09 09:17:08 -0500

    complete quiz1 exercise

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -10,7 +10,12 @@
 
 // TODO: Write a function that calculates the price of an order of apples given
 // the quantity bought.
-// fn calculate_price_of_apples(???) -> ??? { ??? }
+fn calculate_price_of_apples(qty: i8) -> i8 {
+    if qty > 40 {
+        return qty;
+    }
+    qty * 2
+}
 
 fn main() {
     // You can optionally experiment here.
