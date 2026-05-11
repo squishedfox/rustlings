a2a8f07fb61a6293bc654dc289b9e3dd60239966
squishedfox <squishedfox@protonmail.com>
2026-05-10 21:49:45 -0500

    complete 09_strings exercises

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -5,7 +5,7 @@ fn trim_me(input: &str) -> &str {
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this.
-    return String::from(input.to_owned() + " world");
+    return String::from(input.to_owned() + " world!");
 }
 
 fn replace_me(input: &str) -> String {
